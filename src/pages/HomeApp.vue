d9d593eb2dc023eb04e0fd9dbf36e205ea0d0d29
AimanFadil <aimanbello2001@gmail.com>
2024-03-27 10:41:38 +0100

    Scaffholding [ADD]

---
+++ src/pages/HomeApp.vue
@@ -0,0 +1,22 @@
+<script>
+import {store} from '../store.js';
+export default {
+    name: 'HomeApp',
+  data(){
+        return{
+            store,
+        }
+    },
+    
+}
+</script>
+<template lang="">
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/generals.scss' as*;
+
+
+    
+</style>
