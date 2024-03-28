fd832fbbf0e91a6e00953a68c3e5ba5a979ec718
stappost <emanuele.scarpantoni@gmail.com>
2024-03-28 15:19:10 +0100

    add function storage in file js

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,58 +1,36 @@
 <script>
 import { store } from '../store.js';
-import { ref, watch } from "vue";
+import useLocalStorage from '../js/useLocalStorage';
 export default {
     name: 'Cart',
     data() {
         return {
             store,
-            cart: ref(
-                [
-                    {
-                        name: 'pizza',
-                        quantity: 2,
-                        price: 12.34
-                    },
-                    {
-                        name: 'pizza',
-                        quantity: 2,
-                        price: 12.32
-                    },
-                ],
-            )
-
+            my_cart: useLocalStorage(store.cart, 'items')
         }
     },
     mounted() {
-        this.storage()
+
+
     },
     methods: {
-        storage() {
-            const my_storage = window.localStorage.getItem('myItem')
-            this.cart.value = JSON.parse(my_storage)
 
-
-            watch(this.cart, val => {
-                // console.log(item)
-                window.localStorage.setItem('myItem', JSON.stringify(val))
-            }, { deep: true })
-        }
 
     }
 }
 </script>
 <template lang="">
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <div v-for="(item, index) in cart" :key='index'>
-                    {{item.name}}
-                    <input type="text" v-model="item.name">
-                    {{item.quantity}}
-                    {{item.price}}
+
+                <div class="p-5">
+                    <ul class="list-unstyled">
+                        <li v-for="(item, index) in my_cart" :key='index' class="d-flex justify-content-around">
+                            {{item.name}}
+                            <div>quantità: {{item.quantity}}</div> 
+                            prezzo: €{{item.price*item.quantity}}
+                            <input type="text" v-model='item.name'>
+                        </li>
+                    </ul>
                 </div> 
-            </div>
-        </div>
-    </div>
+            
 </template>
 <style lang="scss" scoped></style>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,24 @@
 import { reactive } from 'vue';
+// import { ref } from "vue";
 
 
 export const store = reactive({
 
     Url: 'http://127.0.0.1:8000/api',
+    cart:
+        [
+            {
+                name: 'pizza',
+                quantity: 2,
+                price: 12.34
+            },
+            {
+                name: 'pizza',
+                quantity: 2,
+                price: 12.32
+            },
+        ],
+
 
 
 });
