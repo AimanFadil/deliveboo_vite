e3ad9ee5e28119bf8b427a2de94314ec8da83307
stappost <emanuele.scarpantoni@gmail.com>
2024-03-29 11:41:15 +0100

    add buttons

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,18 +2,36 @@
 import { store } from '../store.js';
 import useLocalStorage from '../js/useLocalStorage';
 export default {
-    name: 'Cart',
+    name: 'Chart',
     data() {
         return {
             store,
-            my_cart: useLocalStorage(store.cart, 'items')
+            my_Chart: useLocalStorage(store.Chart, 'items')
         }
     },
-    mounted() {
-
-
-    },
     methods: {
+        delete_storage() {
+            localStorage.removeItem("items")
+        },
+        remove_article(index) {
+
+            this.my_Chart.splice(index, 1)
+
+        },
+        numberItems() {
+            let totalItem = 0
+            this.my_Chart.forEach((item) => {
+                totalItem += item.quantity
+            })
+            return totalItem
+        },
+        totalPrice() {
+            let totalPrice = 0
+            this.my_Chart.forEach((item) => {
+                totalPrice += item.price * item.quantity
+            })
+            return totalPrice
+        }
 
 
     }
@@ -23,11 +41,19 @@ export default {
 
                 <div class="p-5">
                     <ul class="list-unstyled">
-                        <li v-for="(item, index) in my_cart" :key='index' class="d-flex justify-content-around">
-                            {{item.name}}
+                        <li v-for="(item, index) in my_Chart" :key='index' class="d-flex justify-content-around">
+                            <strong>{{item.name}}</strong>
                             <div>quantità: {{item.quantity}}</div> 
                             prezzo: €{{item.price*item.quantity}}
-                            <input type="text" v-model='item.name'>
+
+                            <button class="delete" @click="remove_article(index)">-</button>
+                            <button>+</button>
+                        </li>
+                        <li>costi di consegna: €1,20</li>
+                        <li>ordine minimo: €20,00</li>
+                        <li>
+                            <button @click="delete_storage()">svuota carrello</button>
+                            <button>Ordinane {{numberItems()}} a €{{totalPrice()}}</button>
                         </li>
                     </ul>
                 </div> 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,10 @@
 import { reactive } from 'vue';
-// import { ref } from "vue";
 
 
 export const store = reactive({
 
     Url: 'http://127.0.0.1:8000/api',
-    cart:
+    Chart:
         [
             {
                 name: 'pizza',
