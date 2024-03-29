69e259c417a6c1d38bb727c0d205de45b3510ad3
stappost <emanuele.scarpantoni@gmail.com>
2024-03-29 11:44:37 +0100

    add chart empty

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -38,25 +38,31 @@ export default {
 }
 </script>
 <template lang="">
+<div v-if="this.my_Chart.length > 0">
 
-                <div class="p-5">
-                    <ul class="list-unstyled">
-                        <li v-for="(item, index) in my_Chart" :key='index' class="d-flex justify-content-around">
-                            <strong>{{item.name}}</strong>
-                            <div>quantità: {{item.quantity}}</div> 
-                            prezzo: €{{item.price*item.quantity}}
+    <div class="p-5">
+        <ul class="list-unstyled">
+            <li v-for="(item, index) in my_Chart" :key='index' class="d-flex justify-content-around">
+                <strong>{{item.name}}</strong>
+                <div>quantità: {{item.quantity}}</div> 
+                prezzo: €{{item.price*item.quantity}}
 
-                            <button class="delete" @click="remove_article(index)">-</button>
-                            <button>+</button>
-                        </li>
-                        <li>costi di consegna: €1,20</li>
-                        <li>ordine minimo: €20,00</li>
-                        <li>
-                            <button @click="delete_storage()">svuota carrello</button>
-                            <button>Ordinane {{numberItems()}} a €{{totalPrice()}}</button>
-                        </li>
-                    </ul>
-                </div> 
+                <button class="delete" @click="remove_article(index)">-</button>
+                <button>+</button>
+            </li>
+            <li>costi di consegna: €1,20</li>
+            <li>ordine minimo: €20,00</li>
+            <li>
+                <button @click="delete_storage()">svuota carrello</button>
+                <button>Ordinane {{numberItems()}} a €{{totalPrice()}}</button>
+            </li>
+        </ul>
+    </div> 
+</div>
+<div v-else>
+    <h4>non ci sono ancora piatti nel carrello</h4>
+    <div>SBRIGATI A ORDINARE ALTRIMENTI CHIUDE E MUORI DI FAME, SAI QUANTO COSTANO I FUNERALI DI QUESTI TEMPI?</div>
+</div>
             
 </template>
 <style lang="scss" scoped></style>
